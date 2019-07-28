ff558d8cd1ab4913768635b79d5bf17c894c27a0
kapjain <32997769+kapjain@users.noreply.github.com>
2019-07-28 15:34:53 +0530

    update

--- Project-Python/DataType/listEx.py
+++ Project-Python/DataType/listEx.py
@@ -2,6 +2,7 @@
 1. Mutable
 2. support slicing
 3. can contain mutable object
+4. can contain duplicate element
 
 List: list is a sequential collection of heterogeneous element and mutable data structure.
 its index start from 0 to n-1 or -n to -1
@@ -109,6 +110,7 @@ print(max(l3))#this
 
 
 #append(object): The append() method appends a passed obj into the existing list.
+#return: None
 l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 l.append(10)
@@ -128,8 +130,10 @@ print(l)#[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, [1, 2, 3], range(11, 15), None, 5]
 
 #l.append(a=3) TypeError: append() takes no keyword arguments
 #l.append() TypeError: append() takes exactly one argument (0 given)
+#l.append(1,2) TypeError: append() takes exactly one argument (2 given)
 
 #insert(index, object)
+# return: None
 l = [1,2,3,4,5]
 l.insert(0,0)
 print(l)#[0, 1, 2, 3, 4, 5]
@@ -146,19 +150,21 @@ print(l)#[0, 1.5, 1, 2, range(6, 8), 7, 3, 4, 5]
 l.insert(9, 10)
 print(l)#[0, 1.5, 1, 2, range(6, 8), 7, 3, 4, 5, 10]
 
-l.insert(20,12)# if the given index in not there then this will add the element at the last position
+l.insert(20,12)# if the given index in not there and index value is positive then this will add the element at the last position
 print(l)#[0, 1.5, 1, 2, range(6, 8), 7, 3, 4, 5, 10, 12]
+
+l.insert(-20,233)# if the given index in not there and index value is negative then this will add the element at the first position
+print(l)#[233,0, 1.5, 1, 2, range(6, 8), 7, 3, 4, 5, 10, 12]
 
 l.insert(11,[1,2,3])
 print(l)#[0, 1.5, 1, 2, range(6, 8), 7, 3, 4, 5, 10, 12, [1, 2, 3]]
 
 # l.insert(2) TypeError: insert() takes exactly 2 arguments (1 given)
-#l.insert(4,range(2,3,0.1)) TypeError: 'float' object cannot be interpreted as an integer
-# list(range(0,10,0.5)) TypeError: 'float' object cannot be interpreted as an integer
 #l.insert() TypeError: insert() takes exactly 2 arguments (0 given)
 
 
-#The extend(seq) method appends the contents of seq to list.
+#The extend(iterable) method appends the contents of seq or itratable object to list.
+# return: None
 l = [0, 1, 2,]
 l.extend((10,11,12))
 print(l)#[0, 1, 2, 10, 11, 12]
@@ -183,14 +189,20 @@ print(l)#[0, 1, 2, 10, 11, 12, 13, 14, 15, 'a', 'b', 1, 2, ('a', 1), ('b', 2), '
 l.extend('h')
 print(l)#[0, 1, 2, 10, 11, 12, 13, 14, 15, 'a', 'b', 1, 2, ('a', 1), ('b', 2), 1, 'h']
 
+l.extend([]) # nothing chnaged
+print(l)#[0, 1, 2, 10, 11, 12, 13, 14, 15, 'a', 'b', 1, 2, ('a', 1), ('b', 2), 1, 'h']
+
 # l.extend(1) TypeError: 'int' object is not iterable
 # l.extend(1,2,3) TypeError: extend() takes exactly one argument (3 given)
 # l.extend() TypeError: extend() takes exactly one argument (0 given)
+# l.extend([5,6],[7,8]) TypeError: extend() takes exactly one argument (2 given)
 
 
 # pop(index = -1), index is an optional parameter
+# return: removed value
 l = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 l.pop()# it will remove the last element from the list and return the value of element
+# 1
 print(l)#[10, 9, 8, 7, 6, 5, 4, 3, 2]
 
 l.pop(3)# if the index is given, it will remove the value of given index
@@ -201,13 +213,24 @@ print(l)#[10, 9, 8, 6, 5, 4, 3, 2]
 #l.pop(-10)IndexError: pop index out of range
 
 
-#remove(object or value), object is mandatory parameter
-l = [10, 8, 6, 4, 2]
+# remove(object), object is mandatory parameter
+# return: None
+l = [10, 8, 6, 4, 2,[1,2,3],8,3]
 l.remove(10)
-print(l)#[8, 6, 4, 2]
+print(l)#[8, 6, 4, 2,[1,2,3],8,3]
+
+l.remove([1,2,3])
+print(l)#[8, 6, 4, 2, 8 ,3]
+
+l.remove(8) # If there the same value in the list, so it will remove whichaver value comes first(index 0,1,2,..n) 
+# and remove only one object
+print(l)#[6, 4, 2, 8 ,3]
+
+l.remove(30) # if the given object is not present in the list. ValueError: list.remove(x): x not in list
 #l.remove() TypeError: remove() takes exactly one argument (0 given)
 #l.remove(10,8) TypeError: remove() takes exactly one argument (2 given)
-#l.remove(30) ValueError: list.remove(x): x not in list
+
+
 
 #clear(), no argument
 l = [4, 5, 3, 9, 1, 10]
