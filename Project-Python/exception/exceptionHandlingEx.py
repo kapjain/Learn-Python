0c6718bcdd91116ef7bd6436d4b31dab408c4050
kapjain <32997769+kapjain@users.noreply.github.com>
2019-08-04 16:42:28 +0530

    update

--- Project-Python/exception/exceptionHandlingEx.py
+++ Project-Python/exception/exceptionHandlingEx.py
@@ -1,36 +1,32 @@
-#Note: try-else and try-finally-else not possible
-# try except
+"""
+Points to remember:
+1. syntax:
 try:
-    a = 1
-    print(a)
+    pass
+except (RuntimeError, TypeError, NameError):
+    pass
+except ZeroDivisionError as e:
+    print (e)
+except NameError:
+    pass
 except:
-    print("error")
-
-#try finally
-try:
-    a = 1
-    print(a)
+    pass
+else:
+    pass
 finally:
-    print("error")
+    pass
 
-#try except else    
-try:
-    a = 1
-    print(a)
-except:
-    print("error")
-else: #The try Ö except statement has an optional else clause, which, when present, must follow all except clauses. It is useful for code that must be executed if the try clause does not raise an exception
-    print("else")
-
-#try except finally    
-try:
-    a = 1
-    print(a)
-except:
-    print("error")
-finally:
-    print("else")
-
+Note: 1. try-else is not possible
+      2. It should follow this order try->except->else->finally.
+      3. default except must be last except statement(only except without exception)
+      4. It can have multiple except statement but only one except statement executed.
+      5. Each except clause can have multiple exceptions
+      6. It can have duplicate except clause but there is no use of duplicate clause
+      7. the except block of code executed when try block throws any exception.
+      7. The else block of code is executed only when there no exception occurs in try block.(When try block does not raise any exception).
+      8. The finally block of code always executed whether we got exception or not.
+      9. Exception() takes no keyword arguments.(Pass only positional argument)
+"""
 
 try :
     a = int(input("enter any no :"))
@@ -39,48 +35,13 @@ try :
     print (c)
 except ZeroDivisionError as x:
     print (x)
-else :#The code in the else-block executes if the code in the try: block does not raise an exception.
+else :
     print("else part")
 finally :
     print ("finally part") 
-    
-
-def this_fails():
-    x = 1/0
-
-try:
-    this_fails()
-except ZeroDivisionError as err:
-    print('Handling run-time error:', err)
-
-
-def functionName( level ):
-    if level <1:
-        raise Exception(level)
-        # The code below to this would not be executed
-        # if we raise the exception 
-    return level
-
-try:
-    l = functionName(-10)
-    print ("level = ",l)
-except Exception as e:
-    print ("error in level argument",e.args[0])
-
-
-
-
-
-
-#A try statement may have more than one except clause, to specify handlers for different exceptions
-# An except clause may name multiple exceptions as a parenthesized tuple, for example:
-
-except (RuntimeError, TypeError, NameError):
-    pass
-
+ 
 
 import sys
-
 try:
     f = open('myfile.txt')
     s = f.readline()
@@ -92,22 +53,43 @@ except ValueError:
 except:
     print("Unexpected error:", sys.exc_info()[0])
     raise
+    
+ 
 
+## Manually raising an exception: raise IntanceOfBaseException(). It must be either a subclass or an instance of BaseException
+def functionName( level ):
+    if level <1:
+        raise Exception("Invalid Level",level) # Exception() takes no keyword arguments.(Pass only positional argument)
+    return level
 
-#User-defined Exceptions
-#Programs may name their own exceptions by creating a new exception class (see Classes for more about Python classes). Exceptions should typically be derived from the Exception class, either directly or indirectly.
-
-class MultiplyByZeroError(ArithmeticError):#Note: user defined exceptions must derive from BaseException
-    def __init__(self, arg):
-        self.args = arg
 try:
-    a = int(input("enter value of a"))
-    if (a*0==0):
-            raise MultiplyByZeroError("Bad hostname")
+    l = functionName(-10)
+    print ("level = ",l)
+except Exception as e:
+    print ("error in level argument",e.args[0],e.args[1]) # error in level argument Invalid Level -10
+    print(e) # ('Invalid Level', -10)
+    print(type(e)) # <class 'Exception'>
+
+
+    
+## User-defined Exceptions: Exceptions should typically be derived from the Exception class, either directly or indirectly.
+class MultiplyByZeroError(Exception):  # Note: user defined exceptions must derive from BaseException
+    def __init__(self, *args):
+        self.args = args
+        # super().__init__(*args)
+
+    def __str__(self):
+        return str(self.args)
+try:
+    if 5 * 0 == 0:
+        raise MultiplyByZeroError("Bad hostname", 5)
 except MultiplyByZeroError as e:
-    print ("user defined exception"+ str(e))
+    print(e) # ('Bad hostname', 5)
+    print(type(e)) # <class '__main__.MultiplyByZeroError'>
 
+    
 
+## creating base exception for all errors:
 class Error(Exception):
     """Base class for exceptions in this module."""
     pass
@@ -140,8 +122,14 @@ class TransitionError(Error):
         self.message = message
         
 
-#Important
-#Defining Clean-up Actions: A finally clause is always executed before leaving the try statement, whether an exception has occurred or not. When an exception has occurred in the try clause and has not been handled by an except clause (or it has occurred in an except or else clause), it is re-raised after the finally clause has been executed. The finally clause is also executed ìon the way outî when any other clause of the try statement is left via a break, continue or return statement. A more complicated example:
+#Defining Clean-up Actions: 
+"""
+A finally clause is always executed before leaving the try statement, whether an exception has occurred or not. When an exception 
+has occurred in the try clause and has not been handled by an except clause (or it has occurred in an except or else clause), it 
+is re-raised after the finally clause has been executed. The finally clause is also executed ‚Äúon the way out‚Äù when any other clause
+of the try statement is left via a break, continue or return statement. A more complicated example:
+"""
+
 def divide(x, y):
     try:
         result = x / y
@@ -149,13 +137,24 @@ def divide(x, y):
         print("division by zero!")
     else:
         print("result is", result)
+        raise Exception("Re-raise exception")
     finally:
         print("executing finally clause")
 
 divide(2, 1)
+"""
+result is 2.0
 
+Traceback (most recent call last):
+executing finally clause
 
+  File "C:/Users/Test/test.py", line 12, in <module>
+    divide(2, 1)
+  File "C:/Users/Test/test.py", line 8, in divide
+    raise Exception("Re-raise exception")
+
+Exception: Re-raise exception
+Process finished with exit code 1
+"""
 divide(2, 0)
-
-
 divide("2", "1")
