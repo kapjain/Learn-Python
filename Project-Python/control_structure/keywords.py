6ff241f5872981cd1904a00015566f64f75210bf
kapjain <32997769+kapjain@users.noreply.github.com>
2019-08-04 07:55:15 +0530

    add

---
+++ Project-Python/control_structure/keywords.py
@@ -0,0 +1,13 @@
+"""
+Python 2: 'exec', 'print' (Total:31)
+Python 3: 'False', 'None', 'True', 'async', 'await', 'nonlocal', (Total:35)
+
+Common keywords: There are 29 keywords which are common in both python 2 and 3.
+['and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except',  'finally', 'for', 'from', 'global', 
+'if', 'import', 'in', 'is', 'lambda', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+"""
+
+import keyword
+
+print(keyword.kwlist)
+print(iskeyword(True))
