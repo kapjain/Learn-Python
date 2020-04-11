07130f35589e95d9ed8e872489fc247d40bd2ded
ncjain <32997769+ncjain@users.noreply.github.com>
2020-04-11 14:11:12 +0530

    updated

--- Project-Python/builtinsFunctionsEx/filterEx.py
+++ Project-Python/builtinsFunctionsEx/filterEx.py
@@ -1,37 +1,31 @@
-#filter(function or None, sequence) list tuple or string
-#return those item of sequence for which function is true if function i None, return the items that are true
-# if the sequence is tuple or string, return the same type or return list.
+# filter(function or None, sequence) : return those item of sequence for which function is true if function is None, return the items that are true
+
+def filterVowels(alphabet):
+    return True if alphabet in ['a', 'e', 'i', 'o', 'u'] else False
+
+alphabets = ['a', 'b', 'd', 'e', 'i', 'j', 'o','j']
+filteredVowels = filter(filterVowels, alphabets)
+print(filteredVowels) # <filter object at 0x02DC7160>
+print(type(filteredVowels)) # <class 'filter'>
+
 
 a = list(filter(lambda d: d!='a','abcd'))
-print(a)
+print(a) # ['b', 'c', 'd']
 
 
 def d(x):
     return True if x !='a' else False
-
 a = list(filter(d,'abcd'))
-print(a)
+print(a) # ['b', 'c', 'd']
 
 
-b= list(filter(None,'abcd'))
-print(b)
+b = filter(None,'abcd ')
+print(list(b)) # ['a', 'b', 'c', 'd', ' ']
 
 
+b = filter(None,{None,1,2,3})
+print(list(b)) # [1, 2, 3]
 
-# list of alphabets
-alphabets = ['a', 'b', 'd', 'e', 'i', 'j', 'o']
 
-# function that filters vowels
-def filterVowels(alphabet):
-    vowels = ['a', 'e', 'i', 'o', 'u']
-
-    if(alphabet in vowels):
-        return True
-    else:
-        return False
-
-filteredVowels = filter(filterVowels, alphabets)
-
-print('The filtered vowels are:')
-for vowel in filteredVowels:
-    print(vowel)
+b = filter(None,{'a':10,'b':20})
+print(list(b))
