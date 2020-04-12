2537e65b6c7981c8aa487d922abf367ccd70c182
ncjain <32997769+ncjain@users.noreply.github.com>
2020-04-12 09:28:37 +0530

    updated

--- Project-Python/builtinsFunctionsEx/sortedEx.py
+++ Project-Python/builtinsFunctionsEx/sortedEx.py
@@ -1,5 +1,6 @@
 """ 
-sorted(iterable[, key][, reverse]):  The sorted() method returns a sorted list from the given iterable. The sorted() method sorts the elements of a given iterable in a specific order - Ascending or Descending.
+sorted(iterable[, key][, reverse]):  The sorted() method returns a sorted list from the given iterable. The sorted() method sorts
+the elements of a given iterable in a specific order - Ascending or Descending.
 
 
 sorted() Parameters
@@ -7,7 +8,7 @@ sorted() Parameters
     2. reverse (Optional) - If true, the sorted list is reversed (or sorted in Descending order)
     3. key (Optional) - function that serves as a key for the sort comparison
 
-Return : sorted() method returns a sorted list from the given iterable.
+Return : sorted list
 
 ord(' ') #32
 ord('a') #97
@@ -16,36 +17,26 @@ ord('A') # 65
 Note: sorting work on ascii value base
 """
 
-# vowels list
-pyList = ['e', 'a', 'u', 'o', 'i']
-print(sorted(pyList))
+l = ['e', 'a', 'u', 'o', 'i']
+print(sorted(l)) # ['a', 'e', 'i', 'o', 'u']
 
-# string 
-pyString = 'Hi This is Kapil'
-print(sorted(pyString)) # [' ', ' ', ' ', 'H', 'K', 'T', 'a', 'h', 'i', 'i', 'i', 'i', 'l', 'p', 's', 's']
+s = 'Hi This is Kapil'
+print(sorted(s)) # [' ', ' ', ' ', 'H', 'K', 'T', 'a', 'h', 'i', 'i', 'i', 'i', 'l', 'p', 's', 's']
 
-# vowels tuple
-pyTuple = ('e', 'a', 'u', 'o', 'i')
-print(sorted(pyTuple))
+t = ('e', 'a', 'u', 'o', 'i')
+print(sorted(t)) # ['a', 'e', 'i', 'o', 'u']
 
+t = {'e', 'a', 'u', 'o', 'i'}
+print("reverse=False : ",sorted(t, reverse=False)) # reverse=False :  ['a', 'e', 'i', 'o', 'u']
 
-# set
-pySet = {'e', 'a', 'u', 'o', 'i'}
-print("reverse=False : ",sorted(pySet, reverse=False)) # reverse=False :  ['a', 'e', 'i', 'o', 'u']
+t = {'e', 'a', 'u', 'o', 'i'}
+print("reverse=True : ",sorted(t, reverse=True)) # reverse=True :  ['u', 'o', 'i', 'e', 'a']
 
-pySet = {'e', 'a', 'u', 'o', 'i'}
-print("reverse=True : ",sorted(pySet, reverse=True)) # reverse=True :  ['u', 'o', 'i', 'e', 'a']
+d = {'e': 1, 'a': 2, 'u': 3, 'o': 4, 'i': 5}
+print(sorted(d, reverse=True)) # ['u', 'o', 'i', 'e', 'a']
 
-# dictionary
-pyDict = {'e': 1, 'a': 2, 'u': 3, 'o': 4, 'i': 5}
-print(sorted(pyDict, reverse=True))
-
-# frozen set
-pyFSet = frozenset(('e', 'a', 'u', 'o', 'i'))
-print(sorted(pyFSet, reverse=True))
-
-
-
+fs = frozenset(('e', 'a', 'u', 'o', 'i'))
+print(sorted(fs, reverse=True)) # ['u', 'o', 'i', 'e', 'a']
 
 
 """
@@ -55,17 +46,37 @@ Based on the results of the key function, you can sort the given iterable.
 
 sorted(iterable, key=len)
 """
+# Builtin function
+t = ['abc','ghjsd', 'a']
+print(sorted(t, key=len))
 
+# Example 1
+s = [
+('Kapil',101,'8871337193'),
+('Vishant',100,'887133723'),
+('Sachin',103,'9871337193'),
+]
+def getroll(item):
+    return item[2]
+print(sorted(s,key = getroll ))
+    
+# Example 2
+k = [
+('Kapil',101),
+('Vishant',100,'887133723'),
+('Sachin',103,'9871337193'),
+]
+def getroll(item):
+    return item[1]
+print(sorted(k,key = getroll ))
 
-# take second element for sort
-def takeSecond(elem):
-    return elem[1]
+# Example 3
+k = [
+('Kapil',101),
+('Vishant',100,'887133723'),
+('Sachin',103,'9871337193'),
+]
 
-# random list
-random = [(2, 2), (3, 4), (4, 1), (1, 3)]
-
-# sort list with key
-sortedList = sorted(random, key=takeSecond)
-
-# print list
-print('Sorted list:', sortedList) #Sorted list: [(4, 1), (2, 2), (1, 3), (3, 4)]
+def getroll(item):
+    return item[2]
+print(sorted(k,key = getroll)) # IndexError: tuple index out of range
