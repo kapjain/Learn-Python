1a1b922e0924d6329409530551759a0d1700ccf2
ncjain <32997769+ncjain@users.noreply.github.com>
2020-04-12 07:39:59 +0530

    updated

--- Project-Python/builtinsFunctionsEx/iter_nextEx.py
+++ Project-Python/builtinsFunctionsEx/iter_nextEx.py
@@ -22,3 +22,31 @@ with open('mydata.txt') as fp:
 #When you run the program, it will open the mydata.txt file in reading mode.
 #Then, the iter(fp.readline, '') in the for loop calls readline (which reads each line in the text file) until the sentinel character,
 #'' (empty string), is reached.
+
+
+## next(iterator, default):
+#iterator - next() retrieves next item from the iterator
+#default (optional) - this value is returned if the iterator is exhausted (there is no next item)
+
+# Return Value from next()
+#The next() function returns the next item from the iterator.
+#If the iterator is exhausted, it returns the default value passed as an argument.
+#If the default parameter is omitted and the iterator is exhausted, it raises StopIteration exception.
+
+random = [5, 9, 'cat']
+random_iterator = iter(random)
+print(random_iterator)
+# Output: 5
+print(next(random_iterator))
+
+# Output: 9
+print(next(random_iterator))
+
+# Output: 'cat'
+print(next(random_iterator))
+
+# Output: None
+print(next(random_iterator, None))
+
+# This will raise Error since iterator is exhausted
+print(next(random_iterator))
