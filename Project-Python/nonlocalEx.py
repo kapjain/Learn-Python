10236baa875a5be13b3a6eee854f9b6b127ba3f2
ncjain <32997769+ncjain@users.noreply.github.com>
2021-05-21 19:09:14 +0530

    updated

--- Project-Python/nonlocalEx.py
+++ Project-Python/nonlocalEx.py
@@ -1,13 +1,67 @@
 a = 15
 def outer_function():
+    a = 5
+    def inner_function():
+        a = 10
+        print("1. functionEx: ",a)
+    inner_function()
+    print("2. functionEx: ",a)
+
+outer_function()
+print("3. functionEx: ",a)
+
+
+a = 15
+def outer_function():
     global a
     a = 5
     def inner_function():
-        #nonlocal a # if you don't declare as nonlocal output will be 10, 5 but if you declare as nonlocal output will be 10, 10
-        
         a = 10
-        print("Inner functionEx: ",a)
+        print("1. functionEx: ",a)
     inner_function()
-    print("Outer functionEx: ",a)
+    print("2. functionEx: ",a)
 
 outer_function()
+print("3. functionEx: ",a)
+
+a = 15
+def outer_function():
+    a = 5
+    def inner_function():
+        nonlocal a
+        a = 10
+        print("1. functionEx: ",a)
+    inner_function()
+    print("2. functionEx: ",a)
+
+outer_function()
+print("3. functionEx: ",a)
+
+
+a = 15
+def outer_function():
+    a = 5
+    def inner_function():
+        global a
+        a = 10
+        print("1. functionEx: ",a)
+    inner_function()    
+    print("2. functionEx: ",a)
+
+outer_function()
+print("3. functionEx: ",a)
+
+# Note: at a time , you can use either nonlocal or global keyword. otherwise it will throw error
+a = 15
+def outer_function():
+    global a
+    a = 5
+    def inner_function():
+        nonlocal a
+        a = 10
+        print("1. functionEx: ",a)
+    inner_function()
+    print("2. functionEx: ",a)
+
+outer_function()
+print("3. functionEx: ",a)
