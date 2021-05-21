f833060f4e6b97caf6da3e030efc05b70db59a8b
ncjain <32997769+ncjain@users.noreply.github.com>
2021-05-21 20:26:02 +0530

    updated

--- Project-Python/DataType/stringEx.py
+++ Project-Python/DataType/stringEx.py
@@ -455,3 +455,29 @@ string = "Python is fun, isn't it"
 
 # splits at last occurence of 'is'
 print(string.rpartition('is')) #('Python is fun, ', 'is', "n't it")
+      
+      
+ 
+ """
+The encode() method returns an encoded version of the string(byte string). Default encoding is the current default string encoding. The errors may be given to set a different error handling scheme.
+
+The decode() method decodes the string (byte string) using the codec registered for encoding. It defaults to the defaultstring encoding.
+str.encode(encoding = 'UTF-8',errors = 'strict')
+Str.decode(encoding = 'UTF-8',errors = 'strict')
+
+Parameters
+
+    encoding − This is the encodings to be used. For a list of all encoding schemes please visit − Standard Encodings.
+
+    errors − This may be given to set a different error handling scheme. The default for errors is 'strict', meaning that encoding errors raise a UnicodeError. Other possible values are 'ignore', 'replace', 'xmlcharrefreplace', 'backslashreplace' and any other name registered via codecs.register_error().
+
+Str = "this is string example....wow!!!";
+Str = Str.encode('base64','strict');
+
+print "Encoded String: " + Str
+print "Decoded String: " + Str.decode('base64','strict')
+
+Encoded String: b'dGhpcyBpcyBzdHJpbmcgZXhhbXBsZS4uLi53b3chISE='
+
+Decoded String: this is string example....wow!!!
+"""
